17f83adb245f275688a31766a6b9b743a9007c16
merajmoe <merajmu10@gmail.com>
2021-07-18 16:40:36 -0500

    Create RegionException.java

---
+++ CS480Demo/src/main/java/region/service/RegionException.java
@@ -0,0 +1,15 @@
+package region.service;
+
+public class RegionException extends Exception {
+
+	public RegionException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public RegionException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
