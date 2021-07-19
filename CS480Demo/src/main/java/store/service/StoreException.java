0485c0eada7b21d3002c3e700a73cf402100a0c0
merajmoe <merajmu10@gmail.com>
2021-07-19 01:03:02 -0500

    Create StoreException.java

---
+++ CS480Demo/src/main/java/store/service/StoreException.java
@@ -0,0 +1,15 @@
+package store.service;
+
+public class StoreException extends Exception {
+
+	public StoreException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StoreException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
