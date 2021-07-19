0939d57d3879f1255f6d6c728a7e965daf4d2b3a
merajmoe <merajmu10@gmail.com>
2021-07-19 04:11:42 -0500

    Update findAll.java

--- CS480Demo/src/main/java/region/web/servlet/findAll.java
+++ CS480Demo/src/main/java/region/web/servlet/findAll.java
@@ -1,5 +1,57 @@
 package region.web.servlet;
 
-public class findAll {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import region.service.RegionService;
+
+/**
+ * Servlet implementation class findAll
+ * 
+ * @author meraj
+ */
+@WebServlet(name = "findAllRegion", urlPatterns = { "/findAllRegion" })
+public class findAll extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public findAll() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RegionService regionservice = new RegionService();
+
+		try {
+			request.setAttribute("RegionList", regionservice.findall());
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		request.getRequestDispatcher("/jsps/region/list.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
 }
