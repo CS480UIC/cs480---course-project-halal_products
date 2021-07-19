4491a5ef64293a3d3e626dec6fffd7d769e844c4
mawahhab83 <54748171+mawahhab83@users.noreply.github.com>
2021-07-19 18:12:19 -0500

    Add and Update functionality has been completed

--- CS480Demo/src/main/java/store/web/servlet/add.java
+++ CS480Demo/src/main/java/store/web/servlet/add.java
@@ -11,7 +11,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.plaf.synth.Region;
 
+import region.service.RegionService;
 import store.domain.Store;
 import store.service.StoreException;
 import store.service.StoreService;
@@ -39,7 +41,9 @@ public class add extends HttpServlet {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		StoreService storeservice = new StoreService();
+		RegionService regionservice = new RegionService();
 		Store store = new Store();
+
 		Map<String, String[]> paramMap = request.getParameterMap();
 		List<String> info = new ArrayList<String>();
 
@@ -48,12 +52,21 @@ public class add extends HttpServlet {
 			info.add(values[0]);
 			System.out.println(name + ": " + Arrays.toString(values));
 		}
-		
-		int id = Integer.parseInt(info.get(0));
-		
-		try {
-			store = storeservice.findById(id);
 
+		int id = 0;
+
+		if (info.size() > 0) {
+			id = Integer.parseInt(info.get(0));
+		}
+
+		try {
+			if (id != 0) {
+				store = storeservice.findById(id);
+			}
+
+			List<region.domain.Region> listregion = regionservice.findall();
+
+			request.setAttribute("listregion", listregion);
 			request.setAttribute("store", store);
 			request.getRequestDispatcher("/jsps/store/add.jsp").forward(request, response);
 		} catch (ClassNotFoundException e) {
@@ -66,7 +79,7 @@ public class add extends HttpServlet {
 			// TODO Auto-generated catch block
 			request.setAttribute("msg", e.getMessage());
 		}
-		
+
 	}
 
 	/**
@@ -77,9 +90,11 @@ public class add extends HttpServlet {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		StoreService storeservice = new StoreService();
+		RegionService regionservice = new RegionService();
 		Map<String, String[]> paramMap = request.getParameterMap();
 		Store store = new Store();
 		List<String> info = new ArrayList<String>();
+		List<region.domain.Region> listregion = null;
 
 		for (String name : paramMap.keySet()) {
 
@@ -87,29 +102,34 @@ public class add extends HttpServlet {
 			info.add(values[0]);
 			System.out.println(name + ": " + Arrays.toString(values));
 		}
-		
-		if(info.get(0) != "") {
+
+		if (info.get(0) != "") {
 			store.setId(Integer.parseInt(info.get(0)));
-		}
-		else {
+		} else {
 			store.setId(0);
 		}
-		
+
 		store.setName(info.get(1));
-		
-	try {
-			if(store.getId() == 0) {
+		store.setAddress(info.get(2));
+		store.setZipcode(Integer.parseInt(info.get(3)));
+		store.setRegion_id(Integer.parseInt(info.get(4)));
+
+		try {
+			listregion = regionservice.findall();
+
+			if (store.getId() == 0) {
 				storeservice.add(store);
-			}
-			else {
+			} else {
 				storeservice.update(store);
 			}
-			
+
 			request.getRequestDispatcher("findAllStore").forward(request, response);
 		} catch (ClassNotFoundException | StoreException e) {
 			// TODO Auto-generated catch block
+
+			request.setAttribute("listregion", listregion);
 			request.setAttribute("msg", e.getMessage());
-			request.setAttribute("region", store);
+			request.setAttribute("store", store);
 			request.getRequestDispatcher("/jsps/store/add.jsp").forward(request, response);
 		} catch (InstantiationException e) {
 			// TODO Auto-generated catch block
