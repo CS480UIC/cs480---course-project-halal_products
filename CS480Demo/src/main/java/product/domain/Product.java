2564828a1eedbc02b14bb55ed60d2f58930a78b8
mawahhab83 <54748171+mawahhab83@users.noreply.github.com>
2021-07-31 14:22:47 -0500

    ToString method added and code formatted

--- CS480Demo/src/main/java/product/domain/Product.java
+++ CS480Demo/src/main/java/product/domain/Product.java
@@ -1,61 +1,65 @@
 package product.domain;
 
 public class Product {
-	
+
 	private int id;
 	private String name;
 	private int category_id;
 	private int manufacturer_id;
 	private int halal_status_id;
 	private String certifications;
-	
-	
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public int getCategory_id() {
 		return category_id;
 	}
-	
+
 	public void setCategory_id(int category_id) {
 		this.category_id = category_id;
 	}
-	
+
 	public int getManufacturer_id() {
 		return manufacturer_id;
 	}
-	
+
 	public void setManufacturer_id(int manufacturer_id) {
 		this.manufacturer_id = manufacturer_id;
 	}
-	
+
 	public int getHalal_status_id() {
 		return halal_status_id;
 	}
-	
+
 	public void setHalal_status_id(int halal_status_id) {
 		this.halal_status_id = halal_status_id;
 	}
-	
+
 	public String getCertifications() {
 		return certifications;
 	}
-	
+
 	public void setCertifications(String certifications) {
 		this.certifications = certifications;
 	}
-}
 
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", category_id=" + category_id + ", manufacturer_id="
+				+ manufacturer_id + ", halal_status_id=" + halal_status_id + ", certifications=" + certifications + "]";
+	}
+}
