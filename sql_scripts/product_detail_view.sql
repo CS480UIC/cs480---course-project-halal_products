160e72dd21bb94060fe649bccf2cdb96ae4418e7
mawahhab83 <54748171+mawahhab83@users.noreply.github.com>
2021-07-31 12:19:50 -0500

    Update product_detail_view.sql

--- sql_scripts/product_detail_view.sql
+++ sql_scripts/product_detail_view.sql
@@ -4,15 +4,17 @@ CREATE
     SQL SECURITY DEFINER
 VIEW `halal_products`.`product_detail` AS
     SELECT 
-        `s`.`id` AS `id`,
-        `s`.`region_id` AS `region_id`,
-        `s`.`name` AS `name`,
-        `s`.`address` AS `address`,
-        `s`.`zipcode` AS `zipcode`,
-        `p`.`manufacturer_id` AS `manufacturer_id`
+        `p`.`name` AS `product_name`,
+        `m`.`name` AS `manufacturer_name`,
+        `s`.`name` AS `store_name`,
+        `p`.`certifications` AS `certifications`,
+        `r`.`name` AS `region_name`
     FROM
         ((((`halal_products`.`product` `p`
-        JOIN `halal_products`.`halal_status` `hs` ON ((`p`.`halal_status_id` = `hs`.`id`)))
         JOIN `halal_products`.`manufacturer` `m` ON ((`p`.`manufacturer_id` = `m`.`id`)))
         JOIN `halal_products`.`product_store` `ps` ON ((`ps`.`product_id` = `p`.`id`)))
         JOIN `halal_products`.`store` `s` ON ((`ps`.`store_id` = `s`.`id`)))
+        JOIN `halal_products`.`region` `r` ON ((`s`.`region_id` = `r`.`id`)))
+    WHERE
+        ((IFNULL(`p`.`halal_status_id`, 0) = 1)
+            AND (`p`.`certifications` IS NOT NULL))
