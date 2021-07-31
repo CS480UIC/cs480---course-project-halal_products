ffce7da616cb617658a4ce29acaf1ec7a873d478
mawahhab83 <54748171+mawahhab83@users.noreply.github.com>
2021-07-31 16:32:33 -0500

    Update getProductDetail.java

--- CS480Demo/src/main/java/product/web/servlet/getProductDetail.java
+++ CS480Demo/src/main/java/product/web/servlet/getProductDetail.java
@@ -1,11 +1,18 @@
 package product.web.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import product.service.ProductService;
 
 /**
  * Servlet implementation class getProductDetail
@@ -39,7 +46,34 @@ public class getProductDetail extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		ProductService productservice = new ProductService();
+
+		Map<String, String[]> paramMap = request.getParameterMap();
+		List<String> info = new ArrayList<String>();
+
+		for (String name : paramMap.keySet()) {
+			String[] values = paramMap.get(name);
+			info.add(values[0]);
+			System.out.println(name + ": " + Arrays.toString(values));
+		}
+
+		String region_name = info.get(0);
+
+		try {
+			request.setAttribute("DetailList", productservice.getProductDetails(region_name));
+
+			request.setAttribute("region_name", region_name);
+			request.getRequestDispatcher("/jsps/product/productDetail.jsp").forward(request, response);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			request.setAttribute("msg", e.getMessage());
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			request.setAttribute("msg", e.getMessage());
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			request.setAttribute("msg", e.getMessage());
+		}
 	}
 
 }
