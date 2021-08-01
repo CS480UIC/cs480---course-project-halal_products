8d20f68ea64a4f5489a9943eef1f38d0729ef1e4
mawahhab83 <54748171+mawahhab83@users.noreply.github.com>
2021-08-01 00:53:25 -0500

    region name added

--- sql_scripts/get_store_by_region.sql
+++ sql_scripts/get_store_by_region.sql
@@ -3,12 +3,13 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `get_store_by_region`(
 )
 BEGIN
 	Select s.name as store_name, Case When ps.availability = 1 Then 'Yes' Else 'No' End 
-    as availability, p.name as product_name, 
+    as availability, p.name as product_name, r.name as region_name,
     (Select name From manufacturer where id = p.manufacturer_id) as manufacturer_name
 	From store s
 	Join product_store ps on ps.store_id = s.id
 	Join product p on ps.product_id = p.id
+    Join region r on s.region_id = r.id
 	Where p.halal_status_id = 1 and (region_id_param is null or 
     s.region_id = region_id_param)
-	Order By s.name, ps.availability, p.name;
+	Order By r.name, ps.availability, s.name, p.name;
 END
