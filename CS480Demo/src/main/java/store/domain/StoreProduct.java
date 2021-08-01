fb1a2d5fcd00d50a70c01d11c74305f1a4651686
mawahhab83 <54748171+mawahhab83@users.noreply.github.com>
2021-07-31 19:19:42 -0500

    Create StoreProduct.java

---
+++ CS480Demo/src/main/java/store/domain/StoreProduct.java
@@ -0,0 +1,75 @@
+package store.domain;
+
+public class StoreProduct {
+
+	private int id;
+	private int region_id;
+	private String name;
+	private String address;
+	private String region_name;
+	private int zipcode;
+	private String product_name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getRegion_id() {
+		return region_id;
+	}
+
+	public void setRegion_id(int region_id) {
+		this.region_id = region_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getRegion_name() {
+		return region_name;
+	}
+
+	public void setRegion_name(String region_name) {
+		this.region_name = region_name;
+	}
+
+	public int getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getProduct_name() {
+		return product_name;
+	}
+
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+
+	@Override
+	public String toString() {
+		return "StoreProduct [id=" + id + ", region_id=" + region_id + ", name=" + name + ", address=" + address
+				+ ", region_name=" + region_name + ", zipcode=" + zipcode + ", product_name=" + product_name + "]";
+	}
+
+}
